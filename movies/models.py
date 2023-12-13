3ba6ed2e0dc9c4aa921b2a35c21b33e44554dff3
Muhammadrizo04 <muhammadrizomuxtorov389@gmail.com>
2023-12-13 14:50:23 +0500

    end create models

--- movies/models.py
+++ movies/models.py
@@ -80,10 +80,39 @@ class MovieShort(models.Model):
         verbose_name_plural="Кадры из фильма"
 
 
-class Rating(models.Model):
+class RatingStar(models.Model):
     value = models.PositiveSmallIntegerField("Значение", default=0)
 
     def __str__(self):
         return self.value
 
     class Meta:
+        verbose_name="Звезда рейтинга"
+        verbose_name_plural="Звезды рейтинга"
+
+class Rating(models.Model):
+    ip = models.CharField("IP адрес", max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="звезда")
+    movie = models.ForeignKey(Movie, on_delete=models.CharField, verbose_name="фильм")
+
+    def __str__(self):
+        return f"{self.star}-{self.movie}"
+
+    class Meta:
+        verbose_name="Рейтинг"
+        verbose_name_plural="Рейтинги"
+
+
+class Reviews(models.Model):
+    email = models.EmailField()
+    name = models.CharField("Имя", max_length=100)
+    text = models.TextField("Собщение", max_length=5000)
+    parent = models.ForeignKey('self', verbose_name="Родитель", on_delete=models.SET_NULL, blank=True, null=True)
+    movie = models.ForeignKey(Movie, verbose_name="фильм", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}-{self.movie}"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
