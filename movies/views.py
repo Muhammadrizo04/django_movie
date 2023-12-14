e9d3500065a45dc0dc0578fc09d482033b9c1226
Muhammadrizo04 <muhammadrizomuxtorov389@gmail.com>
2023-12-14 14:15:53 +0500

    setup views, urls, html

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from .models import *
 
-class MovieView(View):
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movie_list.html", {"movies_list": movies})
+class MoviesView(ListView):
+    model = Movie
+    queryset = Movie.objects.filter(draft=False)
+    # template_name = "movies/movie_list.html"
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = 'url'
