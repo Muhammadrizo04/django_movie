356cf0545655a10c17230ac4b4e287a1cdcd3358
Muhammadrizo04 <muhammadrizomuxtorov389@gmail.com>
2023-12-15 16:38:56 +0500

    add rating

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path("",MoviesView.as_view()),
     path("filter/", FilterMoviesView.as_view(), name="filter"),
+    path("add-rating/", AddStarRating.as_view(), name='add_rating'),
     path("<slug:slug>/",MovieDetailView.as_view(), name="movie_detail"),
     path("review/<int:pk>/", AddReview.as_view(), name="add_review"),
     path("actor/<str:slug>/", ActorView.as_view(), name="actor_detail"),
